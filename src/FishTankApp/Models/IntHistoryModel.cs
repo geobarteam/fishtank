875379ca060f6dd3421601961285668438d20eb9
geobarteam <gvd8@hotmail.com>
2016-01-30 12:39:12 +0100

    initial commit

---
+++ src/FishTankApp/Models/IntHistoryModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FishTankApp.Models
+{
+    public class IntHistoryModel
+    {
+        public IntHistoryModel(DateTime timesStamp, int value)
+        {
+            this.TimeStamp = timesStamp;
+            this.Value = value;    
+        }
+
+        public DateTime TimeStamp { get; set; }
+        public int Value { get; set; }
+    }
+}
---
+++ src/FishTankApp/Services/ISensorDataService.cs
@@ -0,0 +1,17 @@
+﻿using FishTankApp.Models;
+using System.Collections.Generic;
+
+namespace FishTankApp.Services
+{
+    public interface ISensorDataService
+    {
+        IEnumerable<IntHistoryModel> GetWaterTemperatureFahrenheitHistory();
+        IntHistoryModel GetFishMotionPercentage();
+        IEnumerable<IntHistoryModel> GetFishMotionPercentageHistory();
+        IntHistoryModel GetWaterOpacityPercentage();
+        IEnumerable<IntHistoryModel> GetWaterOpacityPercentageHistory();
+        IntHistoryModel GetLightIntensityLumens();
+        IEnumerable<IntHistoryModel> GetLightIntensityLumensHistory();
+        
+    }
+}
